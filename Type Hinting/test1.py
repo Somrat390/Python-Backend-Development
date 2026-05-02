5c965f180cddf9cb368af4f895516cd543c29841
Somrat Sheikh <somratsheikh426@email.com>
2026-05-03 01:49:53 +0600

    Type hinting and Type checking using MYPY

---
+++ Type Hinting/test1.py
@@ -0,0 +1,14 @@
+def create_user(first_name:str, last_name:str, age:int) -> dict:
+    email = f"{first_name.lower()}_{last_name.lower()}@example.com"
+
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "age": age
+
+    }
+
+
+user1 = create_user("Somrat","Sheikh","thirty-eight")
+print(user1)
